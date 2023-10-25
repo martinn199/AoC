4e2dca963b5cf8e7b23a4bb5e1e46680b5d62531
Martin Nibell <marni391@student.liu.se>
2023-10-25 08:11:58 +0200

    Have started to work with Day 5 Part 1

---
+++ Day5/Day5Part1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    /*Define variables and constant*/
+    /*Open the file*/
+    FILE * fPointer;
+    fPointer = fopen("//home//martin//AoC//Day5//input.txt", "r");
+
+    /*If there exist no file, return an error*/
+    if (fPointer == NULL){
+        perror("opening input.txt failed");
+        return 1;
+    }
+    /*Close the file*/
+    fclose(fPointer);
+    return 0;
+}
